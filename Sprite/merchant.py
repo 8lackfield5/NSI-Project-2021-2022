934893e0c771fc4832f1a85086b080d145807420
Remi K-S <83575908+8lackfield5@users.noreply.github.com>
2022-05-24 23:15:25 +0200

    Delete Sprite directory

--- Sprite/merchant.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-merchant = pygame.image.load('Sprite/Merchant_png/merchant.png').convert_alpha()
-shelf = pygame.image.load('Sprite/Merchant_png/shelf.png').convert_alpha()
-
-
-class MerchantMenu():
-  pass
